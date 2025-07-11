4c174975e7d40e4ee040d79a7029becf7f6c9b3a
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2025-07-11 10:22:27 +0530

    Update package dependencies and enhance application setup with Swagger and Pino logging

---
+++ src/cors.ts
@@ -0,0 +1,6 @@
+export default {
+  origin: true,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from '@nestjs/common';
+import { Logger as PinoLogger } from 'nestjs-pino';
+import cors from './cors.js';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
+
+export const GLOBAL_PREFIX = '/ceylon-guide';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+    bufferLogs: true,
+  });
+
+  app.setGlobalPrefix(GLOBAL_PREFIX, {
+    exclude: ['/', 'docs', '/health'],
+  });
+  app.enableCors(cors);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  app.useLogger(app.get(PinoLogger));
+
+  /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
+  const config = new DocumentBuilder()
+    .setTitle('CEYLON GUIDE BACKEND')
+    .setDescription('API for Ceylon Guide Backend')
+    .setVersion('1.0')
+    .setExternalDoc('Postman Collection', '/docs-json')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(GLOBAL_PREFIX + '/docs', app, document);
+  /* eslint-enable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
+
+  const port = process.env.PORT ?? 9062;
+  await app.listen(port).then(() => {
+    const logger = new Logger('main');
+    logger.debug(`Server running on port: ${port} 🚀`);
+  });
 }
-bootstrap();
+void bootstrap();
