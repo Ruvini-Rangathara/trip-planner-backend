c699aad280ce2f52777c24d735eea6bfcb32ae1d
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2025-08-31 20:00:03 +0530

    Enhance Alert module by adding PrismaModule and PrismaService for improved database interaction

--- src/alert/alert.module.ts
+++ src/alert/alert.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AlertService } from './alert.service';
 import { AlertController } from './alert.controller';
+import { PrismaModule } from 'src/common/prisma/prisma.module';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AlertController],
-  providers: [AlertService],
+  providers: [AlertService, PrismaService],
 })
 export class AlertModule {}
